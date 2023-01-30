61f0a1d3fb23bb30223ce03354fd6e105cdcd755
DmitryShon <72316551+DmitryShon@users.noreply.github.com>
2023-01-31 00:17:30 +0300

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,35 @@
+from cfg import accs
+import aiohttp
+import asyncio
+import json
+
+async def get_balance(session,acc):
+    url = f'https://nearblocks.io/api/account/balance?address={acc}'
+    async with session.get(url) as resp:
+        assert resp.status == 200
+        resp_text = await resp.text()
+        balance = round(float(json.loads(resp_text)['balance']),2)
+        sl.update({acc:balance})
+    return resp_text
+
+
+async def get_acc(accounts):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for acc in accounts:
+            task = asyncio.create_task(get_balance(session,acc))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
+async def get_res(accounts):
+    global sl
+    sl = {}
+    output = '<b>        Ежедневный отчет\n(Аккаунты отсортированы по балансу сверху вниз):</b> \n'
+    await get_acc(accounts)
+    for i, acc in enumerate(sorted(sl.items(), key=lambda x: x[1], reverse=True)):
+        if i == len(accs) - 3:
+            output += '\n<b>АУТСАЙДЕРЫ:</b> \n'
+        output += f'\n• <em>{acc[0]}</em> : <b>{acc[1]}Ⓝ</b> \nРаботник: {accs[acc[0]]} '
+        output += '\n——————————————————'
+    return output
+
---
+++ rating_bot.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from cfg import *
+from counter import get_res
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+async def echo_message():
+    await bot.send_message('-1001598054361', await get_res(accs.keys()), parse_mode="html")
+
+
+if __name__ == '__main__':
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(echo_message, "interval", seconds = 10)
+    scheduler.start()
+    executor.start_polling(dp)
