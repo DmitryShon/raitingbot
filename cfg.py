2f30e92e5dfa153e3bba021c89e3b0ea8b28349d
DmitryShon <72316551+DmitryShon@users.noreply.github.com>
2023-01-31 19:25:30 +0300

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -20,18 +20,18 @@ accs = {
     '0x7fff.crowdforces.near': 'Таскер: <b>macneil</b> | Ревьюер:<b>tt</b>',
     'perennial_fanatic.crowdforces.near': 'Таскер: <b>Адронный</b> | Ревьюер: <b>xxxiamgelik</b>',
     'yomikou.crowdforces.near':'<b>Lesha</b>',
-    'curabis.crowdforces.near':'Таскер: <b>Sasha</b> | Ревьюер: <b>romoch</b>',
+    'curabis.crowdforces.near':'Таскер: <b>Sasha</b> | Ревьюер: <b>romochque</b>',
     'karapuz.crowdforces.near': '<b>Poki Mane</b>',
     'hjxcpg.crowdforces.near': 'Таскер: <b>Roman</b> | Ревьюер: <b>tt</b>',
     'sovienok.crowdforces.near': 'Таскер: <b>Roman</b> | Ревьюер: <b>Stef</b>',
-    'anfisa.near': 'Таскер: <b>Андрей</b> | Ревьюер: <b>tt</b>',
+    'anfisa.near': 'Таскер: <b>dd</b> | Ревьюер: <b>tt</b>',
     'michirukagemori.crowdforces.near':'<b>Дань[LSKN]</b>',
     'luckyrandom.crowdforces.near':'<b>Vort</b>',
     'pufik_m22.crowdforces.near': '<b>Stef</b>',
     'ailider.crowdforces.near' : '<b>Алексей</b>',
     'fairywinx_m22.crowdforces.near': '<b>Dmitry</b>',
     'bigpolandbro.crowdforces.near':'<b>LSKN</b>',
-    'kinpa200296.crowdforces.near':'Таскер: <b>d d </b> | Ревьюер: <b>Достоевский</b>',
+    'kinpa200296.crowdforces.near':'<b>Epatage</b>',
     'mindrocker.crowdforces.near':'<b>Vort</b>',
     'nuttyjam.crowdforces.near': 'Таскер: <b>Grief</b> | Ревьюер: <b>tt</b>',
     'yuliay.crowdforces.near': 'Таскер: <b>nikita</b> | Ревьюер: <b>tt</b>',
@@ -39,6 +39,6 @@ accs = {
     'box.crowdforces.near': 'Таскер: <b>Жечко</b> | Ревьюер: <b>tt</b>',
     '2d31856ee3b5c8d75c75c1c5407f77af1415394d070392c63d6028066f965a13':'<b>Epatage</b>',
     'denis_pulyaev.crowdforces.near': 'Таскер: <b>Андрюха</b> | Ревьюер: <b>romoch</b>',
-    'zengshiyuan.crowdforces.near': 'Таскер: <b>Максим</b> | Ревьюер: <b>Stef</b>'
+    'zengshiyuan.crowdforces.near': 'Таскер: <b>Максим</b> | Ревьюер: <b>Алексей Дост.</b>'
 }
 TOKEN = '5896714356:AAFWyj6Mn8gimSlj-BOPB-13coHDBG6xY8k'
